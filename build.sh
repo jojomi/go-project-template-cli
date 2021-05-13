b11290330486e20360c3b799f482a6bba05549ca
jojomi <dev.jojomi@yahoo.com>
2021-05-13 12:11:47 +0200

    Improve portability

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ GIT_STATE=$(git diff --quiet && echo 'clean' || echo 'dirty')
 GIT_REMOTE=$(git config --get remote.origin.url)
 
 # trying to be reproducible
-go install -trimpath -ldflags "-buildid= -X 'main.GitCommit=$GIT_COMMIT' -X 'main.GitBranch=$GIT_BRANCH' -X 'main.GitDate=$GIT_DATE' -X 'main.GitVersion=$GIT_VERSION' -X 'main.GitState=$GIT_STATE'  -X 'main.GitRemote=$GIT_REMOTE'"
+CGO_ENABLED=0 go install -trimpath -ldflags "-buildid= -X 'main.GitCommit=$GIT_COMMIT' -X 'main.GitBranch=$GIT_BRANCH' -X 'main.GitDate=$GIT_DATE' -X 'main.GitVersion=$GIT_VERSION' -X 'main.GitState=$GIT_STATE'  -X 'main.GitRemote=$GIT_REMOTE'"
